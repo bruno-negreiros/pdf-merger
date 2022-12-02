dff01bfad6764708f356e5198b88ecbc817ab986
Bruno Negreiros <bruno.negreiros@itbam.org.br>
2022-12-02 10:00:34 -0400

    refactor: remove duplicated event listener

--- renderer.js
+++ renderer.js
@@ -4,12 +4,6 @@ document.getElementById('dirs').addEventListener('click', () => {
   })
 })
 
-document.getElementById('dirs').addEventListener('click', () => {
-  window.postMessage({
-    type: 'select-dirs'
-  })
-})
-
 document.getElementById('merge-pdfs-button').addEventListener('click', () => {
   window.postMessage({
     type: 'merge-pdfs'
